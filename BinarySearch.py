f195a81d51d3387121f3a09e09dab69c8cdbff9e
Joe Cho <joecho@gmail.com>
2015-07-11 14:05:12 -0400

    sorts

--- BinarySearch.py
+++ BinarySearch.py
@@ -2,7 +2,24 @@ class BinarySearch(object):
     def __init__(self):
         pass
 
-
-         
+    def binSearch(self,alist,item):
+        found = False
+        first = 0
+        last = len(alist) -1
+        midpoint = 0
+        
+        while first <= last and not found:
+            midpoint = (first + last)//2
+            print midpoint
+            if alist[midpoint] == item:
+                found = True
+            else:
+                if item < alist[midpoint]:
+                    last = midpoint - 1
+                else:
+                    first = midpoint + 1
+                    
+        return found
+        
 
 
---
+++ do_binarySearch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from BinarySearch import BinarySearch
+
+bs = BinarySearch()
+
+print bs.binSearch([1,4,5,12,14,19,22],4)
