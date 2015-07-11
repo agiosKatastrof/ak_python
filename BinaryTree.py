38c60f9c57286cb55d7e970c49216ebb78178c14
Joe Cho <joecho@gmail.com>
2015-07-11 18:55:19 -0400

    .

--- BinaryTree.py
+++ BinaryTree.py
@@ -31,3 +31,25 @@ class BinaryTree(object):
         
     def getRootVal(self):
         return self.key
+    
+    def preorder(self):
+        print self.key
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+
+    def postorder(self):
+        if self.leftChild:
+            self.leftChild.postorder()
+        if self.rightChild:
+            self.rightChild.postorder()
+        print self.key
+    
+    def inorder(self):
+        if self.leftChild:
+            self.leftChild.inorder()
+        print self.key
+        if self.rightChild:
+            self.rightChild.inorder()
+            
--- do_binaryTree.py
+++ do_binaryTree.py
@@ -3,8 +3,32 @@
 from BinaryTree import BinaryTree
 
 
-r = BinaryTree('a')
-r.insertLeft('b')
-r.getLeftChild().insertRight('d')
+r = BinaryTree(10)
+r.insertLeft(5)
+r.getLeftChild().insertRight(6)
+r.insertRight(15)
+r.getRightChild().insertLeft(11)
+r.getRightChild().insertRight(100)
 
+print r.getRootVal()
+print r.getLeftChild().getRootVal()
 print r.getLeftChild().getRightChild().getRootVal()
+print r.getRightChild().getRootVal()
+print r.getRightChild().getLeftChild().getRootVal()
+print r.getRightChild().getRightChild().getRootVal()
+
+print
+
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+preorder(r)
+print
+r.preorder()
+print
+r.postorder()
+print
+r.inorder()
