ea8e2035cbc7d5c426fd3318a10f85764d412e36
Joe Cho <joecho@gmail.com>
2015-07-11 22:32:16 -0400

    .

---
+++ BinHeap.py
@@ -0,0 +1,55 @@
+class BinHeap(object):
+    def __init__(self):
+        self.heapList = [0]
+        self.currentSize = 0
+        
+    def percUp(self,i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i // 2]:  # you get the parent by i // 2
+                tmp = self.heapList[i // 2]
+                self.heapList[i // 2] = self.heapList[i] # swap parent with child
+                self.heapList[i] = tmp
+            i = i // 2
+    
+    def insert(self,k):
+        self.heapList.append(k)
+        self.currentSize += 1
+        self.percUp(self.currentSize)
+        
+    def percDown(self,i):
+        while (i * 2) <= self.currentSize: # you get the child by i * 2
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]: # swap child with parent
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = i * 2
+            
+    def minChild(self,i):
+        if i * 2 + 1 > self.currentSize: # no right child
+            return i * 2  # so return left child
+        else:
+            if self.heapList[i*2] < self.heapList[i*2 + 1]:
+                return i * 2   # return left child
+            else:
+                return i * 2 + 1 # return right child
+            
+    def delMin(self):
+        retval = self.heapList[1] # the root node is always the min in a bin heap
+        self.heapList[1] = self.heapList[self.currentSize] # make the last node the root node
+        self.currentSize -= 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+    
+    def buildHeap(self,alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while (i > 0):
+            self.percDown(i)
+            i -= 1
+            
+            
+        
+        
---
+++ do_binheap.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+from BinHeap import BinHeap
+
+l = [9, 5, 6, 2, 3]
+b = BinHeap()
+
+b.buildHeap(l)
+
+print b.currentSize
+print b.heapList
+
+print b.delMin()
+
+print b.heapList
+
+b.insert(7)
+
+print b.heapList
